4a1e6f366beafc545b1c230882d440796b06aa78
Raghav Agarwal <raghagarwal@gmail.com>
2024-06-13 16:44:41 +1000

    added some more swagger rules

--- swagger-doc/routes/convert-pdf.js
+++ swagger-doc/routes/convert-pdf.js
@@ -1,52 +1,162 @@
-// /**
-//  * @swagger
-//  * components:
-//  *   schemas:
-//  *     Convert PDF to UBL:
-//  *       type: object
-//  *       required:
-//  *         - email
-//  *         - password
-//  *         - password-check
-//  *       properties:
-//  *         email:
-//  *           type: string
-//  *           description: The email of the user
-//  *         password:
-//  *           type: string
-//  *         password-check:
-//  *           type: string
-//  *       example:
-//  *         email: 'user@example.com'
-//  *         password: 'password123'
-//  *         password-check: 'password123'
-//  */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ConvertPdfToUbl:
+ *       type: object
+ *       required:
+ *         - PDF
+ *       properties:
+ *         PDF:
+ *           type: string
+ *           format: binary
+ *           description: The PDF file to convert
+ *       example:
+ *         PDF: 'Binary string of the PDF file'
+ */
 
-// /**
-//  * @swagger
-//  * tags:
-//  *   name: PDF Management
-//  *   description: Convert PDF to UBL
-//  * /upload:
-//  *   name: Convert PDF to UBL
-//  *   tags: [Upload]
-//  *   post:
-//  *     summary: Upload a PDF file
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         multipart/form-data:
-//  *           schema:
-//  *             type: object
-//  *             properties:
-//  *               file:
-//  *                 type: string
-//  *                 format: binary
-//  *     responses:
-//  *       200:
-//  *         description: File uploaded successfully
-//  *       400:
-//  *         description: Invalid request
-//  *       500:
-//  *         description: Internal server error
-//  */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     DeleteUblPdf:
+ *       type: object
+ *       required:
+ *         - UBL-id
+ *         - PDF-id
+ *       properties:
+ *         UBL-id:
+ *           type: string
+ *           description: The ID of the UBL to delete
+ *         PDF-id:
+ *           type: string
+ *           description: The ID of the PDF to delete
+ *       example:
+ *         UBL-id: '123'
+ *         PDF-id: '456'
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: E-invoicing (PDF Handling)
+ *   description: The Invoice management service (Conversion)
+ * /convert-pdf-to-ubl:
+ *   post:
+ *     summary: Convert PDF to UBL
+ *     tags: [E-invoicing (PDF Handling)]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/octet-stream:
+ *           schema:
+ *             $ref: '#/components/schemas/ConvertPdfToUbl'
+ *     responses:
+ *       200:
+ *         description: Successful conversion
+ *         content:
+ *           application/xml:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 UBL:
+ *                   type: string
+ *                   format: xml
+ *                 UBL-id:
+ *                   type: string
+ *                 PDF-id:
+ *                   type: string
+ *       400:
+ *         description: Insufficient data in the PDF
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Insufficient data in the PDF, please add more information
+ *                 requiredInformation:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *       404:
+ *         description: Failed to convert PDF to UBL
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Failed to convert PDF to UBL
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Server error, please try again later
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: E-invoicing (PDF Handling)
+ *   description: The Invoice management service (Deletion)
+ * /delete-ubl:
+ *   delete:
+ *     summary: Delete UBL and PDF
+ *     tags: [E-invoicing (PDF Handling)]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/DeleteUblPdf'
+ *     responses:
+ *       200:
+ *         description: Successfully deleted UBL and PDF
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 UBL-id:
+ *                   type: string
+ *                   example: '123'
+ *                 PDF-id:
+ *                   type: string
+ *                   example: '456'
+ *                 message:
+ *                   type: string
+ *                   example: Successfully deleted UBL and PDF
+ *       400:
+ *         description: Bad request errors
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: ["UBL Id does not exist", "PDF Id does not exist", "Unable to delete UBL"]
+ *                 details:
+ *                   type: array
+ *                   items:
+ *                     type: string
+ *                   example: 'As errors'
+ *       500:
+ *         description: Server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Server error, please try again later
+ */
