a68d41f48c47534707eead50bd193c0952198544
Vansh Patel <z5406654@ad.unsw.edu.au>
2024-06-19 23:38:43 +1000

    Dashboard in progress

--- frontend/src/Dashboard/Selector/Selector.js
+++ frontend/src/Dashboard/Selector/Selector.js
@@ -1,4 +1,53 @@
+// const Selector = () => {
+//   return <div>Selector</div>;
+// };
+// export default Selector;
+
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 const Selector = () => {
-  return <div>Selector</div>;
+  // const classes = useStyles();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Perform any necessary logout logic here, like clearing authentication tokens
+    navigate('/');
+  };
+  return (
+    <div>
+      <h1>
+        <img
+          src={`${process.env.PUBLIC_URL}/Path_1929.png`}
+          alt="icon"
+          style={{
+            width: '50px',
+            height: '50px',
+            verticalAlign: 'middle',
+            marginRight: '8px',
+          }}
+        />
+        Hexa Hunks
+      </h1>
+      <nav>
+        <button>
+          <Link to="Dashboard">Dashboard</Link>
+        </button>
+        <button>
+          <Link to="Validate">Validate</Link>
+        </button>
+        <button>
+          <Link to="Invoices">Invoices</Link>
+        </button>
+        <button>
+          <Link to="Settings">Settings</Link>
+        </button>
+        <button>
+          <Link to="Help">Help</Link>
+        </button>
+        <button onClick={handleLogout}>Logout</button>
+      </nav>
+    </div>
+  );
 };
+
 export default Selector;
