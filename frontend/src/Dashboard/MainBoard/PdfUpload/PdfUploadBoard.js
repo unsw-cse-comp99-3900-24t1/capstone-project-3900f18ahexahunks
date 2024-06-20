e03436a4323815ea42bbaa98c3f5ced17b7210c5
Raghav Agarwal <raghagarwal@gmail.com>
2024-06-20 19:30:39 +1000

    user can now upload pdf just need to setup conversion now

--- frontend/src/Dashboard/MainBoard/PdfUpload/PdfUploadBoard.js
+++ frontend/src/Dashboard/MainBoard/PdfUpload/PdfUploadBoard.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { styled } from '@mui/system';
 import UploadBox from './UploadBox';
 import ShowPdf from './ShowPdf';
+import useUserStore from '../../../zustand/useUserStore';
+import { pdfToUblConvert } from '../../../services/api';
+import { useAlert } from '../../../components/AlertError';
 
 const BoardContainer = styled('div')({
   display: 'flex',
@@ -12,12 +15,35 @@ const BoardContainer = styled('div')({
 
 const PdfUploadBoard = () => {
   const [pdfs, setPdfs] = useState([]);
+  const { getUser } = useUserStore();
+  const { showAlert } = useAlert();
 
-  const handleUpload = (event) => {
+  useEffect(() => {
+    console.log('GET Request here to get all the old pdfs');
+  }, []);
+
+  const handleUpload = async (event) => {
     const file = event.target.files[0];
+    const user = getUser();
+    const userId = user._id;
+
     if (file && file.type === 'application/pdf') {
       const fileURL = URL.createObjectURL(file);
       setPdfs((prevPdfs) => [...prevPdfs, fileURL]);
+
+      // Create a FormData object to send the file
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('userId', userId); // Append user ID
+
+      const result = await pdfToUblConvert(formData);
+      if (result.error) {
+        console.error('Error converting PDF to UBL:', result.data);
+      } else {
+        console.log('Conversion successful:', result);
+      }
+    } else {
+      showAlert('Error converting/uploading PDF', 'tomato');
     }
   };
 
