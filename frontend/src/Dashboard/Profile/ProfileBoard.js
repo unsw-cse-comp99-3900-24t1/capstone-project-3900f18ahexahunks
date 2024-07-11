af9a50de1be25924504024e930f319c4a52c4dcc
Raghav Agarwal <raghagarwal@gmail.com>
2024-07-11 16:39:28 +1000

    small css and mobile responsiveness added

--- frontend/src/Dashboard/Profile/ProfileBoard.js
+++ frontend/src/Dashboard/Profile/ProfileBoard.js
@@ -1,128 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import {
-//   Container,
-//   Grid,
-//   TextField,
-//   Button,
-//   Typography,
-//   Avatar,
-//   List,
-//   Paper,
-// } from '@mui/material';
-// import EmailHistoryItem from './EmailHistoryItem';
-// import { getHistoryEmail } from '../../services/api';
-// import { useAlert } from '../../components/AlertError';
-
-// const ProfileBoard = () => {
-//   const [username, setUsername] = useState('John Doe');
-//   const [profilePic, setProfilePic] = useState(null);
-//   const [history, setHistory] = useState([]);
-//   const { showAlert } = useAlert();
-
-//   const handleUsernameChange = (event) => {
-//     setUsername(event.target.value);
-//   };
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const res = await getHistoryEmail();
-//       console.log(res);
-//       if (!res.error) {
-//         setHistory(res); // Assuming res is the array of history email items
-//       } else {
-//         showAlert('Failed to fetch history email:', 'tomato');
-//       }
-//     }
-//     fetchData();
-//   }, []);
-
-//   const handleProfilePicChange = (event) => {
-//     const file = event.target.files[0];
-//     if (file) {
-//       const reader = new FileReader();
-//       reader.onload = () => {
-//         setProfilePic(reader.result);
-//       };
-//       reader.readAsDataURL(file);
-//     }
-//   };
-
-//   const handleDeleteHistory = (index) => {
-//     const newHistory = [...history];
-//     newHistory.splice(index, 1);
-//     setHistory(newHistory);
-//   };
-
-//   const handleOpenReport = (e, item) => {
-//     e.stopPropagation();
-//     console.log(item);
-//   };
-
-//   return (
-//     <Container maxWidth="md">
-//       <Typography variant="h4" gutterBottom>
-//         User Dashboard
-//       </Typography>
-//       <Grid container spacing={3}>
-//         <Grid item xs={12} md={4}>
-//           <Paper elevation={3} style={{ padding: 16 }}>
-//             <Typography variant="h6" gutterBottom>
-//               Update Profile
-//             </Typography>
-//             <Avatar
-//               src={profilePic}
-//               alt="Profile Picture"
-//               sx={{ width: 100, height: 100, marginBottom: 2 }}
-//             />
-//             <input
-//               accept="image/*"
-//               style={{ display: 'none' }}
-//               id="profile-pic-upload"
-//               type="file"
-//               onChange={handleProfilePicChange}
-//             />
-//             <label htmlFor="profile-pic-upload">
-//               <Button variant="contained" color="primary" component="span">
-//                 Upload Profile Picture
-//               </Button>
-//             </label>
-//             <TextField
-//               label="Username"
-//               value={username}
-//               onChange={handleUsernameChange}
-//               fullWidth
-//               margin="normal"
-//             />
-//             <Button variant="contained" color="secondary">
-//               Update
-//             </Button>
-//           </Paper>
-//         </Grid>
-//         <Grid item xs={12} md={8}>
-//           <Paper elevation={3} style={{ padding: 16 }}>
-//             <Typography variant="h6" gutterBottom>
-//               User History
-//             </Typography>
-//             <List>
-//               {history.map((item, index) => (
-//                 <div key={index}>
-//                   <EmailHistoryItem
-//                     handleOpenReport={handleOpenReport}
-//                     item={item}
-//                     handleDeleteHistory={handleDeleteHistory}
-//                     index={index}
-//                   />
-//                 </div>
-//               ))}
-//             </List>
-//           </Paper>
-//         </Grid>
-//       </Grid>
-//     </Container>
-//   );
-// };
-
-// export default ProfileBoard;
 import React, { useEffect, useState } from 'react';
 import {
   Container,
@@ -136,42 +11,50 @@ import {
   IconButton,
 } from '@mui/material';
 import { styled } from '@mui/system';
+import { useNavigate } from 'react-router-dom';
 import EmailHistoryItem from './EmailHistoryItem';
 import { getHistoryEmail } from '../../services/api';
 import { useAlert } from '../../components/AlertError';
 import EditIcon from '@mui/icons-material/Edit';
 import SaveIcon from '@mui/icons-material/Save';
-import DeleteIcon from '@mui/icons-material/Delete';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import CloseIcon from '@mui/icons-material/Close';
 
-const StyledContainer = styled(Container)(({ theme }) => ({
-  marginTop: theme.spacing(4),
-}));
+const StyledContainer = styled(Container)({
+  marginTop: '32px',
+});
 
-const ProfileCard = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(4),
+const ProfileCard = styled(Paper)({
+  padding: '32px',
   textAlign: 'center',
-}));
+});
 
-const ProfileAvatar = styled(Avatar)(({ theme }) => ({
+const ProfileAvatar = styled(Avatar)({
   width: 120,
   height: 120,
   margin: 'auto',
-  marginBottom: theme.spacing(2),
-}));
+  marginBottom: '16px',
+});
 
-const ProfileUploadButton = styled(Button)(({ theme }) => ({
-  margin: theme.spacing(2, 0),
-}));
+const HistoryCard = styled(Paper)({
+  padding: '32px',
+});
 
-const HistoryCard = styled(Paper)(({ theme }) => ({
-  padding: theme.spacing(4),
-}));
-
-const StyledList = styled(List)(({ theme }) => ({
+const StyledList = styled(List)({
   maxHeight: 400,
   overflowY: 'auto',
-}));
+});
+
+const LargeIconButton = styled(IconButton)({
+  position: 'absolute',
+  top: '50px',
+  left: '80px',
+  border: '1px solid #999',
+  '@media (max-width: 640px)': {
+    top: '30px',
+    left: '30px',
+  },
+});
 
 const ProfileBoard = () => {
   const [username, setUsername] = useState('John Doe');
@@ -179,6 +62,7 @@ const ProfileBoard = () => {
   const [history, setHistory] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const { showAlert } = useAlert();
+  const navigate = useNavigate();
 
   const handleUsernameChange = (event) => {
     setUsername(event.target.value);
@@ -227,9 +111,24 @@ const ProfileBoard = () => {
     // Implement your update logic here
   };
 
+  const handleClose = () => {
+    navigate('/dashboard/main');
+  };
+
   return (
     <StyledContainer maxWidth="md">
-      <Typography variant="h4" gutterBottom>
+      <LargeIconButton onClick={handleClose}>
+        <CloseIcon fontSize="large" />
+      </LargeIconButton>
+      <Typography
+        variant="h4"
+        gutterBottom
+        style={{
+          textAlign: 'center',
+          marginTop: '50px',
+          marginBottom: '30px',
+        }}
+      >
         User Dashboard
       </Typography>
       <Grid container spacing={4}>
